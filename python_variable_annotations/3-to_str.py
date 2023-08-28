5abb2f04b9fad0bc51fe666aacc60e18f34fdf05
Nico-dsa <6463@holbertonstudenst.com>
2023-08-28 13:14:50 +0200

    Pycodestyle fix

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -3,6 +3,6 @@
 
 
 def to_str(n: float) -> str:
-    """ Write a type-annotated function to_str that takes a float n 
+    """ Write a type-annotated function to_str that takes a float n
     as argument and returns the string representation of the float. """
     return str(n)
